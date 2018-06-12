e3fce93441b3fa7a755bc34ec1afd104463947d3
chusiyuan <chusiyuan96@gmail.com>
2018-06-12 14:35:38 +0800

    getgirlsnames as keywords and judge weibo

---
+++ TopicsVisualization/weibo_terminater/utils/getgirlsname.py
@@ -0,0 +1,47 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+class GetName:
+
+    def __init__(self):
+        self.headers = {
+            'User-Agent':'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.186 Safari/537.36'
+        }
+
+        self.url = 'http://v.qq.com/biu/101_star_web'
+        self.PATH = 'girlsname.txt'
+
+    # def gethtml(self):
+    #     r = requests.get(self.url, headers=self.headers)
+    #     r.encoding = r.apparent_encoding
+    #     return r.text
+
+    def get_name(self):
+        r = requests.get(self.url, headers=self.headers)
+        r.encoding = r.apparent_encoding
+        names1 = []
+        names2 = []
+        soup = BeautifulSoup(r.text, 'lxml')
+        a = soup.select('#client-entry > div > div > div > div.mod_topic_list > div:nth-of-type(3) > div > div > a:nth-of-type(2)')
+        b = soup.select('#client-entry > div > div > div > div.mod_topic_list > div.mod_pic_list.mod_pic_list_farewell > div > div > a.tit')
+        for name in a:
+            names1.append(name.text)
+        for name in b:
+            names2.append(name.text)
+
+        # with open(self.PATH, 'w') as f:
+        #     f.write(names1)
+
+        return names1, names2
+
+# html = gethtml(url)
+# names1, names2 = get_name(html)
+# print(names1)
+# print(names2)
+# g = GetName()
+# names1, names2 = g.get_name()
+# print(names1)
+# print(names2)
+
+
--- TopicsVisualization/weibo_terminater/utils/textprocesser.py
+++ TopicsVisualization/weibo_terminater/utils/textprocesser.py
@@ -1,12 +1,31 @@
 import re
 from aip import AipNlp
 import time
+import json
+
 
 class TextProcessor:
 
     def __init__(self, path):
         self.path = path
         self.SLEEP_TIME = 0.1
+        self.OUTPUT_FILE_NAME = 'data.json'
+        self.KEY_WORDS =[
+            '创造101', '101', '陈意涵', '段奥娟', '傅菁', '高秋梓', '高颖浠', '赖美云',
+            '李子璇', '李紫婷', '刘人语', '吕小雨', '孟美岐', '戚砚笛', '强东玥', '王菊',
+            '吴宣仪', '吴映香', '徐梦洁', '许靖韵', 'yamy', '杨超越', '杨芸晴', '紫宁',
+            '陈芳语', '范薇', '蒋申', '焦曼婷', '刘丹萌', '鹿小草', '罗怡恬', '罗奕佳',
+            '苏芮琪', '王莫涵', '王婷', '魏瑾', '吴芊盈', '赵尧珂', '杜金雨', '朱天天',
+            '王晴', '热依娜', '于美红', '刘念', '葛佳慧', '丑丑', '菊麟', '刘德熙', '江璟儿',
+            '勾雪莹', '杨冰', '罗智仪', '张溪', 'mena', '马兴钰', '刘思纤', '张楚寒', '向俞星',
+            '吴茜', '尹蕊', '张鑫磊', '陈盈燕', '姜彦汐', '周雪', '吴小萱', '张静萱', '张馨月',
+            '张新洁', '任真', '倪秋云', '杨蕊菡', '杨美琪','毛唯嘉', '林君怡', '林珈安', '邵夏',
+            'Blair', '许诗茵', '潘珺雅', '王雅凛', '胡悦儿', '芮萌', '李天韵', '陈怡凡', '陈语嫣',
+            '王亦然', '夏诗洁','刘宇珊', '朱佳希', '杨美玲', '刘尼夷', '韩丹', '尹柔懿', '罗天舒',
+            '杨晗', '刘佳莹', '吴昀廷', '王曼君', '张瑜纹', '颜可欣', '王珏萌', '邱路晴', '郑丞丞',
+            '山支', '美岐', '宣仪', '小七', '菊姐'
+        ]
+
     def process(self):
         with open(self.path, 'r', encoding='utf-8') as f:
             s = f.read()
@@ -47,13 +66,26 @@ class TextProcessor:
                 while 'error_msg' in analyresult:
                     time.sleep(self.SLEEP_TIME)
                     analyresult = client.sentimentClassify(text)
-                print(analyresult)
+                #print(analyresult)
                 result[i]['positive_prob'] = analyresult['items'][0]['positive_prob']
                 result[i]['sentiment'] = analyresult['items'][0]['sentiment']
                # time.sleep(self.SLEEP_TIME)
 
+            output = {}
+            output['result'] = result
+            with open(self.OUTPUT_FILE_NAME, 'w') as f:
+                json.dump(output, f)
+            # with open(self.OUTPUT_FILE_NAME, 'r') as f:
+            #     t = json.load(f)
+            #
+            #     print(t)
 
-            return(result)
+    def judge_weibo(self, content):
+        for key_word in self.KEY_WORDS:
+            if key_word in content:
+                return True
+
+        return False
 
 
 
